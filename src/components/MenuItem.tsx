43eb4e4ddfdf9589031a7dbc0567c82d9ef27884
gustavo <kg.2k.pl@gmail.com>
2025-04-02 21:26:53 -0400

    Add list of menu items

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,5 +1,14 @@
-export default function MenuItem() {
+import { MenuItem } from "../types"
+
+type MenuItemProps = {
+    item: MenuItem
+}
+
+export default function MenuItem({item} : MenuItemProps) {
   return (
-    <div>MenuItem</div>
+    <button className="border-2 border-red-400 hover:bg-red-200 w-full p-3 flex justify-between">
+        <p>{item.name}</p>
+        <p className="font-black">Bs. {item.price}</p>
+    </button>
   )
 }
