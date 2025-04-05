c22a51375e8ec56b0e632944a1ccc22ec8470095
gustavo <kg.2k.pl@gmail.com>
2025-04-05 02:05:25 -0400

    Pass tip to OrderTotals component and update calculations for tip and total amounts

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -3,12 +3,15 @@ import { OrderItem } from "../types"
 import { formatCurrency } from "../helpers"
 
 type OrderTotalsProps = {
-    order: OrderItem[]
+    order: OrderItem[],
+    tip: number
 }
 
-export default function OrderTotals({ order }: OrderTotalsProps) {
+export default function OrderTotals({ order, tip }: OrderTotalsProps) {
 
     const subTotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+    const tipAmount = useMemo(() => (subTotalAmount * tip), [tip, order])
+    const totalAmount = useMemo(() => subTotalAmount + tipAmount, [tip])
 
     return (
         <>
@@ -18,10 +21,10 @@ export default function OrderTotals({ order }: OrderTotalsProps) {
                     <span className="font-bold">{formatCurrency(subTotalAmount)}</span>
                 </p>
                 <p>Propina: {''}
-                    <span className="font-bold">Bs. 0</span>
+                    <span className="font-bold">{formatCurrency(tipAmount)}</span>
                 </p>
                 <p>Total a pagar: {''}
-                    <span className="font-bold">Bs. 0</span>
+                    <span className="font-bold">{formatCurrency(totalAmount)}</span>
                 </p>
             </div>
 
