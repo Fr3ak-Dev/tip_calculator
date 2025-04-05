28e3b69f1e24688f02a5132e1662afbb92a576ec
gustavo <kg.2k.pl@gmail.com>
2025-04-04 22:34:23 -0400

    Add OrderTotals component to display total amounts and tips

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import MenuItem from "./components/MenuItem"
 import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
 import { menuItems } from "./data/db"
 import useOrder from "./hooks/useOrder" // global state
 
@@ -30,6 +31,8 @@ function App() {
             order={order}
             removeItem={removeItem}
           />
+
+          <OrderTotals/>
         </div>
       </main>
     </>
